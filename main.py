bc0d7b3aa545160976db60a060c366b751a939b2
jlplenio <jan-lukas@plen.io>
2022-02-02 21:37:43 +0100

    new idea

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from spawner import BrowserManager
 target_url = "https://www.twitch.tv/username"
 SPAWNER_THREAD_COUNT = 3
 SPAWNER_THREAD_COUNT = 4
+# todo: Have only one chromedriver instance and listen to requests with driver.request_interceptor
+# asnchronously trigger this requests to many requests_bots with proxy and user-agent
+# and mimic the chromedriver requests. Use only HEAD for GET requests to avoid always any network traffic
 CLOSER_THREAD_COUNT = 10
 DISABLE_CAPTURE = True
 HEADLESS = False
